47ced702b7dfdc680802e4c8b9fc8d552838f9c9
f4rral <f4rral@gmail.com>
2026-04-11 17:11:19 +0300

    8.2 Prepare

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
+        viewBinding = true
     }
 }
 
@@ -48,6 +50,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ kotlin/src/main/java/com/millingcalculator/kotlin/lessons/lesson2/Lesson2.kt
@@ -0,0 +1,64 @@
+package com.millingcalculator.kotlin.lessons.lesson2
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlin.random.Random
+import kotlin.sequences.filter
+import kotlin.sequences.forEach
+import kotlin.sequences.map
+
+val numbers = listOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+
+fun main() {
+    mainSequence()
+//    mainFlow()
+}
+
+fun mainSequence() {
+    println("--- mainSequence start ---")
+
+    val numbersSequence = numbers.asSequence()
+    numbersSequence
+        .filter { it.isRandom()  }
+        .filter { it > 20 }
+        .map {
+            "Number: $it"
+        }
+        .forEach { println(it) }
+
+    println("--- mainSequence end ---")
+}
+
+suspend fun mainFlow() {
+    println("--- mainFlow start ---")
+
+    val numbersFlow = numbers.asFlow()
+    numbersFlow
+        .filter { it.isPrime() }
+        .filter { it > 20 }
+        .map { "Number: $it" }
+        .collect { println(it) }
+
+    println("--- mainFlow end ---")
+}
+
+suspend fun Int.isPrime(): Boolean {
+    if (this <= 1) {
+        return false
+    }
+
+    for (i in 2..this / 2) {
+        delay(50)
+
+        if (this % i == 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+fun Int.isRandom(): Boolean {
+    Thread.sleep(500)
+    return Random.nextBoolean()
+}
