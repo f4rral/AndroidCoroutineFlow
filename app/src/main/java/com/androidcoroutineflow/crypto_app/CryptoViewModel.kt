b7c18984a38526ae1b42765cf3ba700f1fb7a632
f4rral <f4rral@gmail.com>
2026-04-12 18:41:55 +0300

    8.4 Операторы жизненного цикла Flow

--- app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
@@ -19,16 +23,35 @@ class CryptoViewModel : ViewModel() {
     }
 
     private fun loadData() {
-        viewModelScope.launch {
-            while (true) {
+//        viewModelScope.launch {
+//            repository.getCurrencyList()
+//                .onStart {
+//                    val currentState = _state.value
+//
+//                    if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
+//                        _state.value = State.Loading
+//                    }
+//                }
+//                .filter { it.isNotEmpty() }
+//                .onEach {
+//                    _state.value = State.Content(currencyList = it)
+//                }
+//                .collect()
+//        }
+
+        // Равнозначный код что и запуск через viewModelScope.launch
+        repository.getCurrencyList()
+            .onStart {
                 val currentState = _state.value
+
                 if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
                     _state.value = State.Loading
                 }
-                val currencyList = repository.getCurrencyList()
-                _state.value = State.Content(currencyList = currencyList)
-                delay(3000)
             }
-        }
+            .filter { it.isNotEmpty() }
+            .onEach {
+                _state.value = State.Content(currencyList = it)
+            }
+            .launchIn(viewModelScope)
     }
 }
