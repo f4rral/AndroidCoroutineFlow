0d0bf49d6e21984f2662a8638a432843757fc1d2
f4rral <f4rral@gmail.com>
2026-04-14 19:19:58 +0300

    8.6 AsLiveData. OnCompletion.

--- app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
@@ -2,66 +2,33 @@ package com.androidcoroutineflow.crypto_app
 
 import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
-
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    private var job: Job? = null
-    private var isResumed = false
-
-    fun loadData() {
-        isResumed = true
-
-        if (job != null) {
-            return
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter {
+            it.isNotEmpty()
         }
-
-        job = repository.getCurrencyList()
-            .onStart {
-                Log.d("CryptoViewModel", "OnStart")
-
-                _state.value = State.Loading
-            }
-            .filter {
-                it.isNotEmpty()
-            }
-            .onEach {
-                Log.d("CryptoViewModel", "OnEach")
-
-                _state.value = State.Content(currencyList = it)
-            }
-            .onCompletion {
-                Log.d("CryptoViewModel", "OnCompletion")
-            }
-            .launchIn(viewModelScope)
-    }
-
-    fun stopLoading() {
-        viewModelScope.launch {
-            delay(5000)
-
-            if (!isResumed) {
-                job?.cancel()
-                job = null
-
-            } else {
-                isResumed = false
-            }
+        .map {
+            State.Content(it) as State
         }
-    }
+        .onStart {
+            Log.d("CryptoViewModel", "OnStart")
+            emit(State.Loading)
+        }
+        .onEach {
+            Log.d("CryptoViewModel", "OnEach")
+        }
+        .onCompletion {
+            Log.d("CryptoViewModel", "OnCompletion")
+        }
+        .asLiveData()
 }
