0dc7f3e18d71df32acdd79175b4429b05610540a
f4rral <f4rral@gmail.com>
2026-04-16 16:48:50 +0300

    8.11 Промежуточные и кастомные операторы

--- app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/androidcoroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,8 +4,10 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
@@ -14,6 +16,7 @@ import kotlinx.coroutines.launch
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
+    private val loadingFlow = MutableSharedFlow<State>()
 
     val state: Flow<State> = repository.getCurrencyList()
         .filter {
@@ -25,9 +28,15 @@ class CryptoViewModel : ViewModel() {
         .onStart {
             emit(State.Loading)
         }
+        .mergeWith(loadingFlow)
+
+    private fun <T> Flow<T>.mergeWith(another: Flow<T>): Flow<T> {
+        return merge(this, another)
+    }
 
     fun refreshList() {
         viewModelScope.launch {
+            loadingFlow.emit(State.Loading)
             repository.refreshList()
         }
     }
